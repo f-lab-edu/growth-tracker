15cefd35716e54d7af98273ed3f630df115a9607
Jia Lee <jialee8883@gmail.com>
2025-04-15 14:10:42 +0900

    프로젝트 초기 설정 및 HelloWorld 클래스 추가

---
+++ src/main/java/com/jia/growthtracker/GrowthTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jia.growthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthTrackerApplication.class, args);
+	}
+
+}
