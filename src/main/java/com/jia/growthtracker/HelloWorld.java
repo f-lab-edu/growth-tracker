53888659b0ac6865611557d7660d740522318eb0
Jia Lee <jialee8883@gmail.com>
2025-04-15 14:54:23 +0900

    Fix merge conflict and update message

--- src/main/java/com/jia/growthtracker/HelloWorld.java
+++ src/main/java/com/jia/growthtracker/HelloWorld.java
@@ -2,10 +2,6 @@ package com.jia.growthtracker;
 
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, World!");
-=======
-        System.out.println("Hello, Heeve!");
->>>>>>> feature/1
+        System.out.println("Hello, World!");  // 수정된 내용
     }
 }
