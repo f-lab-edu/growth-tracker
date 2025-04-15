56b7b39e96f7596eca607d8e610bce85efa785fe
Jia Lee <jialee8883@gmail.com>
2025-04-15 14:10:42 +0900

    프로젝트 초기 설정 및 HelloWorld 클래스 추가

---
+++ src/test/java/com/jia/growthtracker/GrowthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jia.growthtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
