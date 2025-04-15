bd10bcb1fe0bdb8bc89b2880a30ad20a904e9cb4
Jia Lee <jialee8883@gmail.com>
2025-04-15 14:29:33 +0900

    #1 HelloWorld 메시지 수정

--- src/main/java/com/jia/growthtracker/HelloWorld.java
+++ src/main/java/com/jia/growthtracker/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jia.growthtracker;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Heeve!");
     }
 }
